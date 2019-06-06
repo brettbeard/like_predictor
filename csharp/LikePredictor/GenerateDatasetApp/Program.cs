c64e0e4bbeb76de268399c092cea6f4da91ccbee
bbeard3 <brett.beard@gtri.gatech.edu>
2019-06-06 13:13:58 -0400

    Initial check-in.

---
+++ csharp/LikePredictor/GenerateDatasetApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateDatasetApp
+{
+    using System.IO;
+
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ConvertJsonToCsv(@"..\..\..\..\..\profiles"); 
+
+            var lines = File.ReadAllLines("influencers.csv");
+            foreach (String line in lines)
+            {
+                var tokens = line.Split(',');
+                String url = tokens[2].TrimStart('\"').TrimEnd('\"');
+                Console.WriteLine("{0}", url);
+            }
+        }
+
+        private static void ConvertJsonToCsv(String jsonPath)
+        {            
+            var files = System.IO.Directory.GetFiles(jsonPath, "*.json");
+
+            var outputText = new StringBuilder();
+            foreach (String fileName in files)
+            {
+                JObject jsonObject = JObject.Parse(File.ReadAllText(fileName));
+
+                var userName = jsonObject["username"];
+                var alias = jsonObject["alias"];
+                var url = jsonObject["urlProfile"];
+
+                outputText.AppendFormat("\"{0}\",\"{1}\",\"{2}\"\r\n", alias, userName, url);
+            }
+
+            File.WriteAllText("influencers.csv", outputText.ToString());
+        }
+    }
+}
